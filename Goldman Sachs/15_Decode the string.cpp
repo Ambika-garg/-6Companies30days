1f8695395912fa8d1b0f9394b4ead483a6c4a2a0
Ambika-garg <70703123+Ambika-garg@users.noreply.github.com>
2022-01-08 20:58:40 +0530

    Create 15_Decode the string.cpp

---
+++ Goldman Sachs/15_Decode the string.cpp
@@ -0,0 +1,42 @@
+Problem Link: https://practice.geeksforgeeks.org/problems/decode-the-string2444/1
+-----------------------------------------------------------------------------------
+  
+  
+  
+  class Solution{
+public:
+    string DecodeString(string& s, int &i)
+    {
+        string result;
+        
+        while(i<s.length() && s[i]!=']')
+        {
+            if(isdigit(s[i]))
+            {
+                int k = 0;
+                while(i<s.length() && isdigit(s[i]))
+                {
+                    k = k*10 + s[i++]-'0';
+                }
+                i++;
+                string r = DecodeString(s, i);
+                while(k-- > 0)
+                {
+                    result += r;
+                }
+                i++;
+            }
+            else
+            {
+                result += s[i++];
+            }
+        }
+        return result;
+    }
+    string decodedString(string s){
+        int i=0;
+        return DecodeString(s,i);
+    }
+};
+
+
